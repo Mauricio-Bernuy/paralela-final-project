6a698f2278d2bd9d7bcde04bd2c12a7f93136f4a
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-11-26 18:36:43 -0500

    Update khipuRun.sh

--- khipuRun.sh
+++ khipuRun.sh
@@ -6,9 +6,10 @@ module load python
 # compile serial and omp
 g++ BrokenTriangleSerial.cpp RngStream.cpp -o BrokenTriangleSerial 
 g++ -fopenmp BrokenTriangleOMP.cpp RngStream.cpp -o BrokenTriangleOMP
+
 # run python test scripts
-python3 testerSerial.py > serialResults.txt
-python3 testerOMP.py > OMPResults.txt
+unbuffer python3 testerSerial.py | tee serialResults.txt
+unbuffer python3 testerOMP.py | tee OMPResults.txt
 
 # unload 
 module unload gcc
